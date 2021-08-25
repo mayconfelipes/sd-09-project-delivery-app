abeca6d5b0d933580f012946373186e2bc61e17d
Christopher <chris.tryber@gmail.com>
2021-08-25 17:52:14 -0300

    Created table products

---
+++ back-end/src/database/models/products.js
@@ -0,0 +1,9 @@
+module.exports = (sequelize, DataTypes) => {
+  const products = sequelize.define('products', {
+    name: DataTypes.STRING,
+    price: DataTypes.DECIMAL,
+    url_image: DataTypes.STRING
+  }, { timestamps: false });
+
+  return products;
+};
