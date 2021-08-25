f359120c1426875529ef2990c7a5d95627e7ab0b
jhon <jhonatan.arguello@gmail.com>
2021-08-25 18:01:39 -0300

    Instalaçao do styled-components e estruturaçao de pastas do projeto.

---
+++ front-end/src/pages/login/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <H1>VOCÉ ESTÁ NO LOGIN</H1>
+    </div>
+  );
+};
+
+export default Login;
---
+++ front-end/src/pages/register/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = () => {
+  return (
+    <div>
+      <H1>VOCÉ ESTÁ NO LOGIN</H1>
+    </div>
+  );
+};
+
+export default Register;
