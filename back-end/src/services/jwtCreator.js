d43e2eb0b5c8815091d07075e2ce66882091a745
PedroMCHenrique <pedro.876.ph@gmail.com>
2021-08-26 17:00:40 -0300

    C:/Program Files/Git/register endpoint + jwtCreator

---
+++ back-end/src/services/jwtCreator.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+const jwtCreator = ({ name, email, role }) => {
+  const secret = process.env.JWT_SECRET;
+  const jwtConfig = {
+    expiresIn: '7d',
+    algorithm: 'HS256',
+  };
+  const token = jwt.sign({ name, email, role }, secret, jwtConfig);
+  return token;
+};
+
+module.exports = jwtCreator;
