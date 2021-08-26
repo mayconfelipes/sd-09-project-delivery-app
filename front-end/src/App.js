5121acee3cb24e8a0eeb5b6570b41419c8364134
Luciano Lodi <69854902+lrlodi@users.noreply.github.com>
2021-08-26 10:36:19 -0300

    Revert "[Main group 2][feat][login screen] reqs 1 - 3 "

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-import Login from './pages/Login';
+import rockGlass from './images/rockGlass.svg';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/"><Redirect to="/login" /></Route>
-          <Route path="/login" component={ Login } />
-        </Switch>
-      </BrowserRouter>
+      <span className="logo">TRYBE</span>
+      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
+        Glass
+      </object>
     </div>
   );
 }
