99c82ce45d98f116c2dd1df726a7366df02e5f6a
Diego Fernandes Affonso <diegofernandesaffonso@gmail.com>
2021-08-26 20:14:31 -0300

    vamos la lint

--- front-end/src/components/cardProduct/index.jsx
+++ front-end/src/components/cardProduct/index.jsx
@@ -4,10 +4,10 @@ const CardProduct = (image, name) => {
   const [quantity, setQuantity] = useState(0);
   return (
     <div>
-      <img src={ image } alt={`bebida: ${ image }`} />
+      <img src={ image } alt={ `bebida: ${ image }` } />
       <span>{ name }</span>
       <button
-        type="button"
+        type = "button"
         onClick = { () => {
           if (quantity = 0) return null;
           setQuantity(quantity - 1);
@@ -18,7 +18,7 @@ const CardProduct = (image, name) => {
       <span>{ quantity }</span>
       <button
         type="button"
-        onClick= { () => {
+        onClick = { () => {
           setQuantity(quantity + 1);
         } }
       >
