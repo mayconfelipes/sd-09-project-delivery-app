1541b798e2ff7593d6af612678c982047cc74e97
Christopher <chris.tryber@gmail.com>
2021-08-26 15:00:35 -0300

    Lint

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,18 +1,7 @@
 const express = require('express');
-const { salesProducts, sales, products, users } = require('../database/models');
-
 
 const app = express();
 
 app.get('/coffee', (_req, res) => res.status(418).end());
-app.get('/teste', async (_req, res) => {
-    const product_id = 1
-    const sale_id = 1
-    const quantity = 2
-
-    const teste = await salesProducts.create({ product_id, sale_id, quantity });
-
-    res.status(200).json(teste)
-});
 
 module.exports = app;
