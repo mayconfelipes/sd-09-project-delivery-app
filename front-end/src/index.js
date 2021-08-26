2e2fc41016e9680e9c9e409dcce2526d51681ad9
jhon <jhonatan.arguello@gmail.com>
2021-08-26 17:30:04 -0300

    Estilizaçao inicial da pagina de login.

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Provider from './context/provider';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
