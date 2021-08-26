53138e2e61392d26203136aecbf1cf9a3cbadab8
Diogenes Santos <68401286+SantosDiv@users.noreply.github.com>
2021-08-26 18:37:45 -0300

    feat/req 02

--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import { Link, Redirect } from 'react-router-dom';
 
 function Login() {
-  return (
-    <>
+  const createInput = (name, title) => (
+    <div>
+      <label htmlFor={ name }>{ title }</label>
+      <input
+        id={ name }
+        name={ name }
+        type={ name }
+        data-testid={ `common_login__input-${name}` }
+      />
+    </div>
+  );
 
-    </>
+  return (
+    <div>
+      <form>
+        { createInput('email', 'Login') }
+        { createInput('password', 'Senha') }
+        <button type="submit" data-testid="common_login__button-login">
+          Login
+        </button>
+        <button
+          type="button"
+          data-testid="common_login__button-register">
+          Ainda não tenho conta
+        </button>
+      </form>
+    </div>
   );
 }
 
