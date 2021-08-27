1bf7c95b375e272c3d48d2e352fbfb1a6cc73b92
Fabio <fabiohigoralm2@gmail.com>
2021-08-27 16:13:11 -0300

    Token - login

--- back-end/src/api/service/Login.js
+++ back-end/src/api/service/Login.js
@@ -1,12 +1,13 @@
-// const boom = require('@hapi/boom');
+const boom = require('@hapi/boom');
 const { User } = require('../../database/models');
 
-const login = async ({ email, passord }) => {
-     const loginUser = await User.findAll();
-    // // if (!loginUser || loginUser.passord !== passord) {
-    // //     throw boom.badRequest('campos invalidos');
-    // // }
-     return loginUser;
+const login = async ({ email, password }) => {
+    console.log(email, password);
+    const loginUser = await User.findOne({ where: { email } });
+     if (!loginUser || loginUser.password !== password) {
+         throw boom.badRequest('Senha invalida');
+     }
+    return loginUser;
 };
 
 module.exports = { login };
--- back-end/src/database/models/salesproduct.js
+++ back-end/src/database/models/salesproduct.js
@@ -1,18 +1,18 @@
 module.exports = (sequelize, DataTypes) => {
-  const SalesProduct = sequelize.define("SalesProduct", {
+  const SalesProduct = sequelize.define('SalesProducts', {
     quantity: DataTypes.INTEGER
   }, { timestamp: false });
 
   SalesProduct.associate = (models) => {
     models.Sale.belongsToMany(models.Product, {
       as: 'products',
-      through: SalesProducts,
+      through: "SalesProducts",
       foreignKey: 'sale_id',
       otherkey: 'product_id',
     });
     models.Product.belongsToMany(models.Sale, {
       as: 'sales',
-      through: SalesProducts,
+      through: "SalesProducts",
       foreignKey: 'product_id',
       otherkey: 'sale_id',
     });
