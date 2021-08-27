ec334a99383b18ae600cb7af31e3dbcc9ef2e875
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-08-27 16:19:02 -0300

    Fix: atualizaçao e resoluçao de conflitos

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import Register from './components/pages/Register';
-<<<<<<< HEAD
-=======
-import Login from './pages/Login';
->>>>>>> 550a041f3fcd046bc8b4f7f45a1f018063b2b166
+import Register from './pages/Register';
 import Orders from './pages/Orders';
 import './App.css';
 import Products from './pages/Products';
@@ -15,13 +11,8 @@ function App() {
     <Switch>
       <Route exact path="/">
         <Redirect to="/login" />
-<<<<<<< HEAD
       </Route>
       <Route path="/register" component={ Register } />
-=======
-      <Route path="/register" component={ Register } />
-      <Route exact path="/customer/products" />
->>>>>>> 550a041f3fcd046bc8b4f7f45a1f018063b2b166
       <Route exact path="/customer/products" component={ Products } />
       <Route exact path="/customer/checkout" />
       <Route exact path="/customer/orders/:id" />
