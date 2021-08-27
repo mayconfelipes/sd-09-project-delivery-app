91e9f38647ccc2b63c7f8700bacba7d5aff31049
Diogenes Santos <68401286+SantosDiv@users.noreply.github.com>
2021-08-27 16:32:49 -0300

    fix/lint

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
 const app = express();
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,7 +13,5 @@ const middlewares = require('../middlewares');
 
 app.use('/login', controllers.login, middlewares.error);
 app.get('/coffee', (_req, res) => res.status(418).end());
-
-// app.use('/login', controller.login);
 
 module.exports = app;
