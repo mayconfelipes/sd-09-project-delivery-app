e6240af9ac8524bd600e13b4e6176581d32ece29
Diego Fernandes Affonso <diegofernandesaffonso@gmail.com>
2021-08-27 19:12:37 -0300

    Form validator

--- front-end/src/services/formValidator.js
+++ front-end/src/services/formValidator.js
@@ -3,6 +3,14 @@ const formValidator = (email, password, name) => {
   const minNameLenght = 12;
   const minPasswordLenght = 6;
 
+  if (name === undefined) {
+    console.log('lint');
+    if (!emailRegex.test(email)
+    || password.length < minPasswordLenght) {
+      return false;
+    }
+  }
+
   if (
     (name && name.length < minNameLenght)
     || !emailRegex.test(email)
