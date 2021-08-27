af5b82efef4d4fc3294585eb0bebf27fad9ba86c
JRGCast <joao.ralph1@gmail.com>
2021-08-27 19:29:19 -0300

    {Update: back Pequena alteraçao no script de commit^^}

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 read -p "Qual mensagem do commit? " msg
-read -p "Alteração back ou front?" bOrF
+read -p "Alteração back ou front? " bOrF
 
-theMsg="Update: $bOrF $msg"
+theMsg={"Update: $bOrF $msg"^^}
 git add .
 git commit -m "$theMsg"
 
