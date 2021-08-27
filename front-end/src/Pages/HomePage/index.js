dc64f8af034af60f9907db6b0d459b4a36c2e171
Joao Victor <30628255+Galaraz@users.noreply.github.com>
2021-08-27 16:48:26 -0300

    começando no front

---
+++ front-end/src/Pages/HomePage/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const HomePage = () => <div className="home">Home page</div>;
+
+export default HomePage;
---
+++ front-end/src/Pages/index.js
@@ -0,0 +1,5 @@
+import Login from './LoginPage';
+import HomePage from './HomePage';
+import NotFound from './NotFound';
+
+export { Login, HomePage, NotFound };
