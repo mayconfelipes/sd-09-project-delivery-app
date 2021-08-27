4e4aa8ab132b8dba39b130e1c82720925a11c7dc
mayconfelipes <mayconfelipe.dev@gmail.com>
2021-08-27 18:39:51 -0300

    Verifica existencia do usuario - retorna Token caso exista - verifica senha encriptada

--- back-end/src/controllers/loginController.js
+++ back-end/src/controllers/loginController.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken');
+const md5 = require('md5');
 const { Users } = require('../database/models');
 require('dotenv').config();
-
+ 
 const jwtConfig = {
   algorithm: 'HS256',
   expiresIn: 600000,
@@ -10,14 +11,18 @@ const jwtConfig = {
 const secret = 'secret_key';
 
 const login = async (req, res) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
   try {
     const exists = await Users.findOne({ where: { email } });
     if (!exists) {
       return res.status(400).json({ message: 'Invalid fields' });
     }
-      const token = jwt.sign({ data: exists.displayName }, secret, jwtConfig);
-      return res.status(200).json({ message: 'Login successful', token });
+      const encPassword = md5(password);
+      if (encPassword === exists.password) {
+        const token = jwt.sign({ data: exists.displayName }, secret, jwtConfig);
+        return res.status(200).json({ message: 'Login successful', token });
+      }
+      return res.status(400).json({ message: 'Wrong Password' });
   } catch (e) {
     return res.status(500).json({ message: 'Internal Error', error: e.message });
   }
