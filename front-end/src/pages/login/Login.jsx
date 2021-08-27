a9cc73718e84cbb664c00de1b9c5224259ba0ca6
Christopher <chris.tryber@gmail.com>
2021-08-27 17:22:57 -0300

    contectanco login front com back 2.0

--- front-end/src/pages/login/Login.jsx
+++ front-end/src/pages/login/Login.jsx
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { loginAPI } from '../../services/loginAPI';
 
 function Login() {
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const isInputValid = () => {
+    const passwordminLength = 6;
+
+    const isDisabled = email.match(/\S+@\S+\.\S+/)
+      && password.length >= passwordminLength;
+
+    setIsButtonDisabled(!isDisabled);
+  };
+
+  const canUserLogin = async () => {
+    const loginBody = { email, password };
+    const responseLogin = await loginAPI(loginBody);
+    console.log(responseLogin);
+  };
+
   return (
-    <form>
+    <form
+      onSubmit={ (e) => {
+        e.preventDefault();
+        canUserLogin();
+      } }
+    >
       <div className="login-input">
         <label htmlFor="email">
-          Enter you email:
+          Login:
           <input
             type="email"
             name="email"
+            onChange={ (e) => {
+              setEmail(e.target.value);
+              isInputValid();
+            } }
             data-testid="common_login__input-email"
-            // value={ email }
             placeholder="exemplo@exemplo.com"
             required
           />
@@ -18,20 +46,29 @@ function Login() {
       </div>
       <div className="login-input">
         <label htmlFor="name">
-          Enter you Name:
+          Senha
           <input
-            type="text"
-            name="name"
+            type="password"
+            name="password"
+            onChange={ (e) => {
+              setPassword(e.target.value);
+              isInputValid();
+            } }
             data-testid="common_login__input-password"
-            // value={ name }
             placeholder="Barak Obama"
             required
           />
         </label>
       </div>
-      <button type="button" data-testid="common_login__button-login">LOGIN</button>
       <button
         type="button"
+        data-testid="common_login__button-login"
+        disabled={ isButtonDisabled }
+      >
+        LOGIN
+      </button>
+      <button
+        type="submit"
         data-testid="common_login__button-register"
       >
         AINDA NÃO TENHO CONTA
--- front-end/src/services/loginAPI.js
+++ front-end/src/services/loginAPI.js
@@ -1,16 +1,25 @@
-const loginAPI = (body) => {
-  const endPoint = 'localhost:3001/login';
-  const init = {
-    method: 'POST',
-    body,
-    mode: 'cors',
-    cache: 'default',
-  };
+const URLBase = 'http://localhost:3001';
 
-  const request = fetch(endPoint, init);
-  const response = request.json();
-  console.log(response);
-  return response;
+const loginAPI = async (loginBody) => {
+  try {
+    const URL = `${URLBase}/login`;
+    const requestOptions = {
+      mode: 'cors',
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(loginBody),
+    };
+    const request = await fetch(URL, requestOptions);
+    const response = await request.json();
+    return response;
+  } catch (error) {
+    const errorObj = {
+      message: 'something bad happened here',
+      error: error.message,
+      status: 500,
+    };
+    console.log(errorObj);
+  }
 };
 
 module.exports = {
