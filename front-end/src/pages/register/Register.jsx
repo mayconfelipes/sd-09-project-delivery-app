663466e6d68f1ffedf6b5e1c7ed429c3b457ef68
Enio Nicael <enionicael@gmail.com>
2021-08-27 20:27:13 -0300

    Refatoracao, remocao de comentarios e logs.

--- front-end/src/pages/register/Register.jsx
+++ front-end/src/pages/register/Register.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-// import { Link } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
 import api from '../../service/axiosApi';
 
@@ -41,7 +40,7 @@ function Register() {
       })
       .catch((err) => {
         setShowError(true);
-        console.log(err);
+        console.log('[Erro] >', err.response.data.message);
       });
   };
 
