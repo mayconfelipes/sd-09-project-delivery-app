b0a53258c220a0ec982d76a58f92c27b689b0f0e
Daniela Muller <danielamuller20@gmail.com>
2021-08-27 16:22:49 -0300

    card datatestid

--- front-end/src/components/ProductCard/index.jsx
+++ front-end/src/components/ProductCard/index.jsx
@@ -2,16 +2,36 @@ import React from 'react';
 import P from 'prop-types';
 import style from './productCard.module.scss';
 
-const ProductCard = ({ price, image, description }) => (
+const ProductCard = ({ id, price, image, description }) => (
   <div className={ style.productCardContainer }>
-    <span>{`R$ ${price}`}</span>
-    <img src={ image } alt={ description } />
+    <span data-testid={ `customer_products__element-card-price-${id}` }>{price}</span>
+    <img
+      data-testid={ `customer_products__img-card-bg-image-${id}` }
+      src={ image }
+      alt={ description }
+    />
     <div className={ style.productCard }>
-      <h2>{description}</h2>
+      <h2 data-testid={ `customer_products__element-card-title-${id}` }>{description}</h2>
       <div className={ style.quantityContainer }>
-        <button type="button" className={ style.decrement }>-</button>
-        <input type="text" value="0" />
-        <button type="button" className={ style.increment }>+</button>
+        <button
+          data-testid={ `customer_products__button-card-rm-item-${id}` }
+          type="button"
+          className={ style.decrement }
+        >
+          -
+        </button>
+        <input
+          data-testid={ `customer_products__input-card-quantity-${id}` }
+          type="text"
+          // value="0"
+        />
+        <button
+          data-testid={ `customer_products__button-card-add-item-${id}` }
+          type="button"
+          className={ style.increment }
+        >
+          +
+        </button>
       </div>
     </div>
   </div>
@@ -20,7 +40,8 @@ const ProductCard = ({ price, image, description }) => (
 export default ProductCard;
 
 ProductCard.propTypes = {
-  price: P.number.isRequired,
+  price: P.string.isRequired,
   image: P.string.isRequired,
   description: P.string.isRequired,
+  id: P.number.isRequired,
 };
