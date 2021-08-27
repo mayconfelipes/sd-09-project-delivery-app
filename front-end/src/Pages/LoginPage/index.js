f2d88643d99f6b48c0cd1cf1bf9570a0df429ee3
Renato Feitosa <pythaqua@gmail.com>
2021-08-27 19:23:36 -0300

    chore(front-end): Set styled components up

--- front-end/src/Pages/LoginPage/index.js
+++ front-end/src/Pages/LoginPage/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
+// Title
+// Button
+// Inputs
+// Logo
+// ErrorMessages
+
 const LoginPage = () => <div className="login">Login page</div>;
 
 export default LoginPage;
---
+++ front-end/src/styles/GlobalStyle.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+const url = 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Roboto');
+
+  @font-face {
+    font-family: Roboto, sans-serif;
+    src: url(${url});
+  }
+
+  html, body {
+    font-family: Roboto, sans-serif;
+  }
+
+  *, *:before, *:after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
