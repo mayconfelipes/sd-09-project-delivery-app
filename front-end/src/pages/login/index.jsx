f254b52743b3b4c694a1290426af97f3baf895c7
jhon <jhonatan.arguello@gmail.com>
2021-08-27 15:19:39 -0300

    Implementaçao do controle de formulario.

--- front-end/src/pages/login/index.jsx
+++ front-end/src/pages/login/index.jsx
@@ -1,18 +1,28 @@
-import React, { useContext } from 'react';
-import { Redirect } from 'react-router-dom';
+import React, { useContext, useEffect } from 'react';
+import { Redirect } from 'react-router';
 import { Main, Logo, LoginButton } from './styled';
 import context from '../../context';
 import logo from '../../images/logo.png';
 import FormRender from '../../components/form';
+import formValidator from '../../services/formValidator';
 
 const Login = () => {
   const { form, setForm } = useContext(context);
-  const { redirect } = form;
+  const { email, password, redirect } = form;
 
   const logOn = () => {
     // Fazer o fetch para a api e setar o localStorage
     setForm({ ...form, redirect: !redirect });
   };
+
+  const verifyForm = () => {
+    const isValid = formValidator(email, password, _);
+    setForm({ ...form, enableButton: isValid });
+  };
+
+  useEffect(() => {
+    verifyForm();
+  });
 
   return (
     <Main>
---
+++ front-end/src/services/formValidator.js
@@ -0,0 +1,15 @@
+const formValidator = (email, password, name) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+$/;
+  const minNameLenght = 12;
+  const minPasswordLenght = 6;
+
+  if (
+    (name && name.length < minNameLenght)
+    || !emailRegex.test(email)
+    || password.length < minPasswordLenght
+  ) return false;
+
+  return true;
+};
+
+export default formValidator;
