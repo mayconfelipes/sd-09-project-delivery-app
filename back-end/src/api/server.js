ba102c379d3596f57e174958b8a3d492ce0b8b65
mayconfelipes <mayconfelipe.dev@gmail.com>
2021-08-27 17:17:40 -0300

    em andamento

--- back-end/src/api/server.js
+++ back-end/src/api/server.js
@@ -1,11 +1,5 @@
 const port = process.env.PORT || 3001;
 const app = require('./app');
-const { readFile } = require('fs');
-
-const secret = readFile('./jwt.evaluation.txt', (err, data) => {
-  if (err) throw err;
-  return(data);
-});
 
 app.listen(port);
-console.log(`Api rodando na porta ${secret}`);
+console.log(`Api rodando na porta ${port}`);
