d05063818f59ea33139294b587cbadf890809afe
Rafael Reis <rafaelrnascimento2006@gmail.com>
2021-08-26 21:22:57 -0300

    refatorando codigo

--- back-end/src/api/controllers/sale.js
+++ back-end/src/api/controllers/sale.js
@@ -24,13 +24,15 @@ route.post('/', [
   rescue(async (req, res) => {
     const sale = await Sale.create(req.body);
     res.status(201).json(sale);
-})]);
+  }),
+]);
 
 route.get('/', [
   rescue(async (_req, res) => {
     const salesList = await Sale.findAll();
     res.status(200).json(salesList);
-})]);
+  }),
+]);
 
 // route.use(validateToken);
 
@@ -38,19 +40,22 @@ route.get('/:id', [
   rescue(async (req, res) => {
     const sale = await Sale.findOne(req.params);
     res.status(200).json(sale);
-})]);
+  }),
+]);
 
 route.put('/:id', [
   validate(saleValidator),
   rescue(async (req, res) => {
     const sale = await Sale.update(req.body, req.params);
     res.status(200).json(sale);
-})]);
+  }),
+]);
 
 route.delete('/:id', [
   rescue(async (req, res) => {
     await Sale.destroy(req.params);
     res.status(204).json();
-})]);
+  }),
+]);
 
 module.exports = route;
