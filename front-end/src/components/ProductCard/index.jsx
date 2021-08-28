a80775fa36c519b02a13b3ceb87c8c876ec20ffc
Daniela Muller <danielamuller20@gmail.com>
2021-08-28 09:45:44 -0300

    productCard

--- front-end/src/components/ProductCard/index.jsx
+++ front-end/src/components/ProductCard/index.jsx
@@ -1,41 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import P from 'prop-types';
 import style from './productCard.module.scss';
 
-const ProductCard = ({ id, price, image, description }) => (
-  <div className={ style.productCardContainer }>
-    <span data-testid={ `customer_products__element-card-price-${id}` }>{price}</span>
-    <img
-      data-testid={ `customer_products__img-card-bg-image-${id}` }
-      src={ image }
-      alt={ description }
-    />
-    <div className={ style.productCard }>
-      <h2 data-testid={ `customer_products__element-card-title-${id}` }>{description}</h2>
-      <div className={ style.quantityContainer }>
-        <button
-          data-testid={ `customer_products__button-card-rm-item-${id}` }
-          type="button"
-          className={ style.decrement }
+const ProductCard = ({ id, price, image, description }) => {
+  const [quantity, setQuantity] = useState(0);
+  const onInputChange = (ed, event) => {
+    console.log(event.target.value, ed);
+  };
+  return (
+    <div className={ style.productCardContainer }>
+      <span data-testid={ `customer_products__element-card-price-${id}` }>{price}</span>
+      <img
+        data-testid={ `customer_products__img-card-bg-image-${id}` }
+        src={ image }
+        alt={ description }
+      />
+      <div className={ style.productCard }>
+        <h2
+          data-testid={ `customer_products__element-card-title-${id}` }
         >
-          -
-        </button>
-        <input
-          data-testid={ `customer_products__input-card-quantity-${id}` }
-          type="text"
-          // value="0"
-        />
-        <button
-          data-testid={ `customer_products__button-card-add-item-${id}` }
-          type="button"
-          className={ style.increment }
-        >
-          +
-        </button>
+          {description}
+        </h2>
+        <div className={ style.quantityContainer }>
+          <button
+            data-testid={ `customer_products__button-card-rm-item-${id}` }
+            type="button"
+            className={ style.decrement }
+            onClick={ () => setQuantity(quantity - 1) }
+          >
+            -
+          </button>
+          <input
+            data-testid={ `customer_products__input-card-quantity-${id}` }
+            type="text"
+            value={ quantity }
+            onChange={ (event) => onInputChange(id, event) }
+          />
+          <button
+            data-testid={ `customer_products__button-card-add-item-${id}` }
+            type="button"
+            className={ style.increment }
+            onClick={ () => setQuantity(quantity + 1) }
+          >
+            +
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ProductCard;
 
