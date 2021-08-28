41f9dc94cb03d0e7f32e02077912f11abe88f7d2
brenno-calado <brenno.calado@gmail.com>
2021-08-28 09:04:45 -0300

    undo seeder password hash

--- back-end/src/database/seeders/20210826132014-users.js
+++ back-end/src/database/seeders/20210826132014-users.js
@@ -6,19 +6,19 @@ module.exports = {
       [{
         name: 'Delivery App Admin',
         email: 'adm@deliveryapp.com',
-        password: 'e10adc3949ba59abbe56e057f20f883e',
+        password: 'a4c86edecc5aee06eff8fdeda69e0d04',
         role: 'administrator',
       },
       {
         name: 'Fulana Pereira',
         email: 'fulana@deliveryapp.com',
-        password: 'e10adc3949ba59abbe56e057f20f883e',
+        password: '3c28d2b0881bf46457a853e0b07531c6',
         role: 'seller',
       },
       {
         name: 'Cliente Zé Birita',
         email: 'zebirita@email.com',
-        password: 'e10adc3949ba59abbe56e057f20f883e',
+        password: '1c37466c159755ce1fa181bd247cb925',
         role: 'customer',
       },
       ], {});
