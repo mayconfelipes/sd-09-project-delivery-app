2cbf72bbba90ddc7b125b76d3b1c11554786726a
julianafatsil <julianafatsil@gmail.com>
2021-08-28 21:18:58 -0300

    Refactoring code, and adjustes arrow function decrement

--- front-end/src/utils/actionAddDec.js
+++ front-end/src/utils/actionAddDec.js
@@ -3,11 +3,39 @@ const dataTestId = require('./dataTestIds');
 
 const testid = dataTestId.default;
 
+const setProductLocalStorage = (idProduct, qtdProduct) => {
+  const name = document.querySelector(`[data-testid='${testid[15]}${idProduct}']`)
+    .innerText;
+  const urlImage = document.querySelector(
+    `[data-testid='${testid[17]}${idProduct}']`,
+  ).src;
+  const price = parseFloat(
+    document
+      .querySelector(`[data-testid='${testid[16]}${idProduct}']`)
+      .innerText.split('R$')[1]
+      .replace(',', '.'),
+  );
+  const total = parseFloat((price * qtdProduct).toFixed(2));
+
+  productData = {
+    id: idProduct,
+    name,
+    urlImage,
+    price,
+    quantity: qtdProduct,
+    total,
+  };
+  setCarrinhoLocalStorage(productData);
+  document.querySelector(`[data-testid='${testid[21]}']`)
+    .innerText = getTotalCartLocalStorage();
+};
+
 const decrement = (event) => {
   const idProduct = event.target.id.split('-')[1];
   const qtdProduct = document.getElementById(`qtd-${idProduct}`).value - 1;
   if (qtdProduct >= 0) {
     document.getElementById(`qtd-${idProduct}`).value = qtdProduct;
+    setProductLocalStorage(idProduct, qtdProduct);
   }
 };
 
@@ -18,24 +46,8 @@ const increment = (event) => {
     10,
   );
   qtdProduct += 1;
-  document.getElementById(`qtd-${idProduct}`).value = qtdProduct;
-  const price = parseFloat(document
-    .querySelector(`[data-testid='${testid[16]}${idProduct}']`)
-    .innerText.split('R$')[1].replace(',', '.'));
-  productData = {
-    id: idProduct,
-    name: document.querySelector(`[data-testid='${testid[15]}${idProduct}']`)
-      .innerText,
-    urlImage: document.querySelector(
-      `[data-testid='${testid[17]}${idProduct}']`,
-    ).src,
-    price,
-    quantity: qtdProduct,
-    total: parseFloat((price * qtdProduct).toFixed(2)),
-  };
-  setCarrinhoLocalStorage(productData);
-  document.querySelector(`[data-testid='${testid[21]}']`)
-    .innerText = getTotalCartLocalStorage();
+  document.getElementById(`qtd-${idProduct}`).value = qtdProduct; // alterando elem tela
+  setProductLocalStorage(idProduct, qtdProduct);
 };
 
 module.exports = { decrement, increment };
