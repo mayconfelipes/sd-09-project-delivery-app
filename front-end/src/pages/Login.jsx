eb8a355805a42d12f409a6aa650dd64756a88d64
Carine Becker <caarine.m@gmail.com>
2021-08-30 18:16:30 -0300

    feat: tela de login

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -5,6 +5,25 @@ function Login() {
   return (
     <div className="App">
       <h1>Login</h1>
+      <input
+        type="email"
+        name="login-email"
+        data-testid="common_login__input-email"
+      />
+      <input
+        type="password"
+        name="login-password"
+        data-testid="common_login__input-password"
+      />
+      <button type="submit" data-testid="common_login__button-login">
+        LOGIN
+      </button>
+      <button type="button" data-testid="common_login__button-register">
+        Ainda não tenho conta
+      </button>
+      <div>
+        <p data-testid="common_login__element-invalid-email">email inválido</p>
+      </div>
     </div>
   );
 }
