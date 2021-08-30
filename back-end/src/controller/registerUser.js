de8e2fa1a68136ce7867c7ddd7cd905f099aaca5
Diogenes Santos <68401286+SantosDiv@users.noreply.github.com>
2021-08-30 18:15:15 -0300

    fix/req 12, 13, 14

--- back-end/src/controller/registerUser.js
+++ back-end/src/controller/registerUser.js
@@ -9,9 +9,9 @@ const validateUser = require('../middlewares/validateUser');
 router.post('/', [
   validateUser,
   rescue(async (req, res, _next) => {
-    await service.registerUser(req.body);
+    const user = await service.registerUser(req.body);
 
-    return res.status(201).json({ message: 'Created' });
+    return res.status(201).json(user);
   }),
 ]);
 
--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import './css/navbar.css';
 import { useHistory } from 'react-router-dom';
+import './css/navbar.css';
 
 function Navbar() {
   const { name } = JSON.parse(localStorage.getItem('user'));
@@ -12,37 +12,38 @@ function Navbar() {
   };
 
   return (
-    <nav>
-      <ul>
-        <li>
-          <a
-            data-testid="customer_products__element-navbar-link-products"
-            href="/customer/products"
-          >
-            Produtos
-          </a>
-        </li>
-        <li>
-          <a
-            href="/customer/orders"
-            data-testid="customer_products__element-navbar-link-orders"
-          >
-            Meus Pedidos
-          </a>
-        </li>
-        <li data-testid="customer_products__element-navbar-user-full-name">
+    <nav className="navbar">
+      <div>
+        <a
+          data-testid="customer_products__element-navbar-link-products"
+          href="/customer/products"
+        >
+          Produtos
+        </a>
+      </div>
+      <div>
+        <a
+          href="/customer/orders"
+          data-testid="customer_products__element-navbar-link-orders"
+        >
+          Meus Pedidos
+        </a>
+      </div>
+      <div data-testid="customer_products__element-navbar-user-full-name">
+        <p>
           {name}
-        </li>
-        <li>
-          <button
-            type="button"
-            data-testid="customer_products__element-navbar-link-logout"
-            onClick={ handleClick }
-          >
-            Sair
-          </button>
-        </li>
-      </ul>
+        </p>
+      </div>
+      <div>
+        <button
+          href="/"
+          type="button"
+          data-testid="customer_products__element-navbar-link-logout"
+          onClick={ handleClick }
+        >
+          Sair
+        </button>
+      </div>
     </nav>
   );
 }
