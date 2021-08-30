7ed84b83c52698b734ef3b3158eb78529dc5396c
Lucas Silva <martssilvapro@gmail.com>
2021-08-30 16:20:18 -0300

    correção no componente header

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -3,29 +3,31 @@ import { Link } from 'react-router-dom';
 import Button from './button';
 import '../styles/header.css';
 
-const Header = () => (
-
-  <header className="header">
-    <nav className="headerNav">
-      <ul className="headerList">
-        <li data-testid="customer_products__element-navbar-link-products">
-          <Link to="/customer/products">Produtos</Link>
-        </li>
-        <li data-testid="customer_products__element-navbar-link-orders">
-          <Link to="/customer/orders">Meus pedidos</Link>
-        </li>
-        <li
-          data-testid="customer_products__element-navbar-user-full-name"
-        >
-          Cicrano da Silva
-        </li>
-      </ul>
-    </nav>
-    <Button
-      name="Sair"
-      data-testid="customer_products__element-navbar-link-logout"
-    />
-  </header>
-);
+const Header = () => {
+  const userData = JSON.parse(localStorage.getItem('user'));
+  return (
+    <header className="header">
+      <nav className="headerNav">
+        <ul className="headerList">
+          <li data-testid="customer_products__element-navbar-link-products">
+            <Link to="/customer/products">Produtos</Link>
+          </li>
+          <li data-testid="customer_products__element-navbar-link-orders">
+            <Link to="/customer/orders">Meus pedidos</Link>
+          </li>
+          <li
+            data-testid="customer_products__element-navbar-user-full-name"
+          >
+            { userData.name }
+          </li>
+        </ul>
+      </nav>
+      <Button
+        name="Sair"
+        data-testid="customer_products__element-navbar-link-logout"
+      />
+    </header>
+  );
+};
 
 export default Header;
