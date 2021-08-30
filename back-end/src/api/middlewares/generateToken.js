88717e52fd4e54145964345cf03a09f7e0b3ecd9
Thiago Granville <thiagogranville@gmail.com>
2021-08-30 15:23:51 -0300

    [MAIN GROUP 8][REFACTOR][Back-End][Implementa uso de path no gerador de token]

--- back-end/src/api/middlewares/generateToken.js
+++ back-end/src/api/middlewares/generateToken.js
@@ -1,16 +1,22 @@
 const jwt = require('jsonwebtoken');
 const fs = require('fs').promises;
+const path = require('path');
 
 const readSecret = async () => {
- const secret = await fs.readFile('../back-end/jwt.evaluation.key', 'utf8', (err, data) => {
-  if (err) {
-    console.error(`Erro: ${err}`);
-    process.exit(1);
-  } 
-  return data;
- });
+  const secret = await fs.readFile(
+    path.join(__dirname, '..', '..', '..', 'jwt.evaluation.key'),
+    'utf8',
+    (err, data) => {
+      if (err) {
+        console.error(`Erro: ${err}`);
 
- return secret;
+        process.exit(1);
+      }
+      return data;
+    },
+  );
+
+  return secret;
 };
 
 const jwtConfig = {
