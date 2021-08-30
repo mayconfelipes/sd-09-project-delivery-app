0effd966cce731b420be2adc09b5e2bac7b1e507
felipe <felipesantiagolds@gmail.com>
2021-08-30 15:39:54 -0300

    Navbar lint

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 
-const NavBar = () => {
-  return (
-    <div>
-      <ul>
-        <li>PRODUTOS</li>
-        <li>MEUS PEDIDOS</li>
-        <li>Usuario</li>
-        <li>Sair</li>
-      </ul>
-    </div>
-   );
-}
+const NavBar = () => (
+  <div>
+    <ul>
+      <li>PRODUTOS</li>
+      <li>MEUS PEDIDOS</li>
+      <li>Usuario</li>
+      <li>Sair</li>
+    </ul>
+  </div>
+);
 
 export default NavBar;
