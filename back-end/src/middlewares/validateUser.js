ec70cde6e738fa8e2a7b0d4a1a3d4b51f22a5176
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-08-30 16:28:39 -0300

    Fix: erro

--- back-end/src/middlewares/validateUser.js
+++ back-end/src/middlewares/validateUser.js
@@ -1,13 +1,13 @@
-const { User } = require('../database/models/user');
+const { User } = require('../database/models');
 
 const validateUser = async (req, _res, next) => {
-  const { name, email  } = req.body;
+  const { name, email } = req.body;
 
   const nameUser = await User.findOne({ where: { name }});
   const emailUser = await User.findOne({ where: { email }});
 
   if(nameUser || emailUser) {
-    return next({ Status: 409, message: 'User already registered'});
+    return next({ statusCode: 409, message: 'User already registered'});
     }
     return next();
 };
