491230bdd5383946b797979c959ea7ca6c3c7771
Diego Fernandes Affonso <diegofernandesaffonso@gmail.com>
2021-08-30 18:36:28 -0300

    refatoração logica para inserção data-testid

--- front-end/src/components/navbar/index.jsx
+++ front-end/src/components/navbar/index.jsx
@@ -1,18 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Navbar = (abas, user) => {
-  const { tabs } = abas;
+const Navbar = ({ abas, user }) => {
   console.log('lint eu te amo');
   return (
     <nav>
-      {tabs.map((aba) => {
+      {abas.map((aba) => {
         console.log('lint');
-        return <a key={ aba } href="##">{ aba }</a>;
+        return (
+          <a
+            data-testid={ aba.split('/')[1] }
+            key={ aba }
+            href="##"
+          >
+            { aba.split('/')[0] }
+          </a>);
       })}
-      <span>{ user }</span>
-      <button type="button">Sair</button>
+      <span
+        data-testid="customer_products__element-navbar-user-full-name"
+      >
+        { user }
+      </span>
+      <button
+        type="button"
+        data-testid="customer_products__element-navbar-link-logout"
+      >
+        Sair
+      </button>
     </nav>
   );
+};
+
+Navbar.propTypes = {
+  abas: PropTypes.node.isRequired,
+  user: PropTypes.string.isRequired,
 };
 
 export default Navbar;
--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -12,14 +12,8 @@ export const api = axios.create(
 export const loginApi = (stringUser) => {
   api.defaults.headers.authorizarion = stringUser;
 
-  const token = api.get(`${URL}login`, json)
+  const token = api.post(`${URL}login`)
     .then((response) => response)
     .catch((err) => console.log(err));
   return token;
-};
-
-export const registerUserApi = (data) => {
-  api.post('/register', { ...data })
-    .then((response) => response.data)
-    .catch((err) => console.log(err));
 };
