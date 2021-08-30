f94f847c70bd0bea930f19ceb9742827fe1ee88f
jhon <jhonatan.arguello@gmail.com>
2021-08-30 18:31:54 -0300

    Inserindo botao register no login e logica de login.

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -4,22 +4,9 @@ const headers = require('./setUpFetch.json');
 
 const URL = 'http://localhost:3001/';
 
-export const api = axios.create(
+const Api = axios.create(
   { baseURL: URL },
   { headers },
 );
 
-export const loginApi = (stringUser) => {
-  api.defaults.headers.authorizarion = stringUser;
-
-  const token = api.get(`${URL}login`, json)
-    .then((response) => response)
-    .catch((err) => console.log(err));
-  return token;
-};
-
-export const registerUserApi = (data) => {
-  api.post('/register', { ...data })
-    .then((response) => response.data)
-    .catch((err) => console.log(err));
-};
+export default Api;
