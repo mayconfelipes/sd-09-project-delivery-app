0f32a5de5fcf1408427cee89505bf4ea46ae7a75
Uilton <itoclick@gmail.com>
2021-08-30 22:05:22 -0300

    cards

--- front-end/src/pages/Produtos.jsx
+++ front-end/src/pages/Produtos.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from '../components/Header';
+import { mapCards } from '../services/services';
 
 const Produtos = () => (
   <section>
     <Header />
+
   </section>
 );
 
