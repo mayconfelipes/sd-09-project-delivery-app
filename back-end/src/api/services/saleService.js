252735ef6c4c43e5022624f6cec294bb3d0c4912
lcgoandete <lcgoandete@hotmail.com>
2021-08-31 16:58:14 -0300

    feat create endpoint sales

--- back-end/src/api/services/saleService.js
+++ back-end/src/api/services/saleService.js
@@ -1,10 +1,27 @@
-const { User } = require('../../database/models');
+const moment = require('moment');
+const { Sale, User, SaleProduct } = require('../../database/models');
 const { isValidToken } = require('./utils/tokenValidate');
+
+const PENDENTE = 'pendente';
 
 const create = async (authorization, sale) => {
   isValidToken(authorization);
-  const result = await User.create(sale);
-  return result;
+  
+  const { products, email, ...newSale } = sale;
+  const user = await User.findOne({ where: { email } });
+  const userId = user.dataValues.id;
+
+  newSale.userId = userId;
+  newSale.saleDate = moment().format();
+  newSale.status = PENDENTE;
+
+  const result = await Sale.create(newSale);
+  const saleId = result.dataValues.id;
+  
+  products.forEach(async ({ productId, quantity }) => SaleProduct.create(
+    { saleId, productId, quantity },
+  ));
+  return { saleId };
 };
 
 module.exports = {
