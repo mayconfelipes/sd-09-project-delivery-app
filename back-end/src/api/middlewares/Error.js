da5bad5811e2457cdbf78eb824e6747944e4d666
Pedro Henrique <pedro@lifelink.com.br>
2021-08-31 14:40:41 -0300

    fix req 1 1

--- back-end/src/api/middlewares/Error.js
+++ back-end/src/api/middlewares/Error.js
@@ -1,4 +1,4 @@
-const error = (err, req, res, _next) => {
+const error = (err, _req, res, _next) => {
     if (err.isJoi) {
         return res.status(400).json({
             message: err.details[0].message,
@@ -7,7 +7,7 @@ const error = (err, req, res, _next) => {
 
     if (err.isBoom) {
         const { payload } = err.output;
-        return res.status(404).json({ message: payload.message });
+        return res.status(404).json({ message: payload.message, "hasToken": false });
     }
     return res.status(500)
         .json({ message: err.message });
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { LoginProvider } from './context/loginContext';
 import Login from './pages/Login';
 //  import rockGlass from './images/rockGlass.svg';
@@ -12,7 +12,11 @@ function App() {
         <Switch>
           <Route path="/produtos" component={ Login } />
           <Route path="/login" component={ Login } />
-          <Route exact path="/" component={ Login } />
+          <Route
+            exact
+            path="/"
+            render={ () => (<Redirect to="/login" />) }
+          />
         </Switch>
 
         {/* <span className="logo">TRYBE</span>
