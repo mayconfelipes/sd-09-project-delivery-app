6eb107550a6d359ff9cd560ad2960b078033dd9d
Diogenes Santos <68401286+SantosDiv@users.noreply.github.com>
2021-08-31 13:29:27 -0300

    fix/jwt.evaluation.key

--- back-end/src/utils/generateToken.js
+++ back-end/src/utils/generateToken.js
@@ -1,6 +1,11 @@
 const jwt = require('jsonwebtoken');
+const path = require('path');
 
-const { SECRET_KEY } = process.env;
+const SECRET_KEY = require("fs")
+  .readFileSync(
+    path.join(__dirname, '..', '..', 'jwt.evaluation.key'),
+    { encoding: "utf-8" })
+  .trim();
 
 module.exports = (id) => {
   const jwtConfig = {
