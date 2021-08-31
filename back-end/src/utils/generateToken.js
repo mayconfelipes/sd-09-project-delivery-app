c7de3e31ffe8c29f26a48a13ebb2b4ad9eb8b7f2
Diogenes Santos <68401286+SantosDiv@users.noreply.github.com>
2021-08-31 13:39:16 -0300

    fix/lint

--- back-end/src/utils/generateToken.js
+++ back-end/src/utils/generateToken.js
@@ -1,10 +1,11 @@
 const jwt = require('jsonwebtoken');
 const path = require('path');
 
-const SECRET_KEY = require("fs")
+const SECRET_KEY = require('fs')
   .readFileSync(
     path.join(__dirname, '..', '..', 'jwt.evaluation.key'),
-    { encoding: "utf-8" })
+    { encoding: 'utf-8' },
+  )
   .trim();
 
 module.exports = (id) => {
