5a74abc1ca702c18b0bff9721352f9df6d8bf527
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-08-31 19:20:12 -0300

    criados arquivos para pagina vendedor

--- front-end/src/pages/Routes.js
+++ front-end/src/pages/Routes.js
@@ -9,6 +9,7 @@ import Login from './Login/Login';
 import Register from './Register/Register';
 import Admin from './Admin/Admin';
 import Products from './Products/Products';
+import Seller from './Seller/Seller';
 
 function Routes() {
   return (
@@ -17,6 +18,7 @@ function Routes() {
         <Route path="/login" component={ Login } />
         <Route path="/register" component={ Register } />
         <Route exact path="/admin/manage" component={ Admin } />
+        <Route exact path="seller/orders" component={ Seller } />
         <Route path="/customer/checkout" component={ Products } />
         <Route path="/customer/products" component={ Products } />
         <Redirect from="/" to="/login" />
