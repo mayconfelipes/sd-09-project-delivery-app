69301cdd19d5e354ee4ab6827b7a93987b62e057
Matheus Martino <matheus.martino97@gmail.com>
2021-08-31 15:32:31 -0300

    Adicionando endpoint PUT /sale.

--- back-end/schemas/index.js
+++ back-end/schemas/index.js
@@ -1,7 +1,11 @@
 const removePassword = require('./removePassword');
 const generateError = require('./generateError');
+const seedSalesProducts = require('./seedSalesProducts');
+const calculateTotalPrice = require('./calculateTotalPrice');
 
 module.exports = {
   removePassword,
   generateError,
+  seedSalesProducts,
+  calculateTotalPrice,
 };
--- back-end/src/services/Sale.js
+++ back-end/src/services/Sale.js
@@ -1,8 +1,10 @@
-/* eslint-disable camelcase */
-/* eslint-disable max-lines-per-function */
 const Joi = require('joi');
-const { generateError } = require('../../schemas');
 const { Sale, SaleProduct } = require('../database/models');
+const {
+  seedSalesProducts,
+  calculateTotalPrice,
+  generateError,
+} = require('../../schemas');
 const Product = require('./Product');
 
 const RegisterSchema = Joi.object({
@@ -13,26 +15,15 @@ const RegisterSchema = Joi.object({
   cart: Joi.array().required(),
 });
 
-const calculateTotalPrice = (cart) => {
-  const totalPrice = cart
-    .reduce(async (acc, { productId, quantity }) => {
-      const newAcc = await acc;
-      const product = await Product.findById(productId);
+const UpdateSchema = Joi.object({
+  id: Joi.number().required(),
+  status: Joi.string().required(),
+});
 
-      return newAcc + (product.dataValues.price * quantity);
-    }, 0);
-    
-  return totalPrice;
-};
+const findById = async (id) => {
+  const sale = await Sale.findByPk(id)
 
-const seedSalesProducts = (saleId, cart) => {
-  cart.forEach(({ productId, quantity }) => {
-    SaleProduct.create({
-      saleId,
-      productId,
-      quantity,
-    });
-  });
+  return sale;
 };
 
 const register = async (saleInfo) => {
@@ -41,7 +32,7 @@ const register = async (saleInfo) => {
   if (error) throw generateError(422, error.message);
 
   const { userId, sellerId, deliveryAddress, deliveryNumber, cart } = saleInfo;
-  const totalPrice = await calculateTotalPrice(cart);
+  const totalPrice = await calculateTotalPrice(Product, cart);
 
   const sale = await Sale.create({
     userId,
@@ -52,11 +43,26 @@ const register = async (saleInfo) => {
     status: 'Pendente',
   });
 
-  seedSalesProducts(sale.id, cart);
+  seedSalesProducts(SaleProduct, sale.id, cart);
 
   return { sale: sale.dataValues };
 };
 
+const update = async ({ id, status }) => {
+  const { error } = UpdateSchema.validate({ id, status });
+
+  if (error) throw generateError(422, error.message);
+
+  await Sale.update({ status }, { where: { id } });
+
+  const sale = await findById(id);
+
+  return {
+    sale,
+  };
+};
+
 module.exports = {
   register,
+  update,
 };
