f5bb57c7cd32df80872df7ef88d670dc05f66780
LucioOSilva <lucioosilva00@gmail.com>
2021-08-30 23:20:06 -0300

    added route '/images'

--- back-end/src/controllers/routes/router.js
+++ back-end/src/controllers/routes/router.js
@@ -3,6 +3,7 @@ const pingController = require('../pingController');
 const registerController = require('../registerController');
 const loginController = require('../loginController');
 const productsController = require('../productsController');
+const imagesController = require('../imagesController');
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.use('/ping', pingController);
 router.use('/login', loginController);
 router.use('/register', registerController);
 router.use('/products', productsController);
+router.use('/images', imagesController);
 
 module.exports = router;
---
+++ back-end/src/services/image.js
@@ -0,0 +1,16 @@
+const errorObj = require('../utils/errorObj');
+const statusCode = require('../utils/statusCode');
+const path = require('path');
+
+const getImage = async (imgName) => {
+  try {
+    const composedPath = path.resolve(path.resolve(__dirname + `/../images/${imgName}`));
+    return composedPath;
+  } catch (error) {
+    return errorObj("Image name or path doesn't exists", statusCode.badRequest);
+  }
+};
+
+module.exports = {
+  getImage,
+};
