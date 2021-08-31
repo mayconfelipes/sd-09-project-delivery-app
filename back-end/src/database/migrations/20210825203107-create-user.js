cb64c59143e7db9bb559b212b3f22a806d12a7b3
Pedro Henrique <pedro@lifelink.com.br>
2021-08-31 13:17:08 -0300

    fix

--- back-end/src/database/migrations/20210825203107-create-user.js
+++ back-end/src/database/migrations/20210825203107-create-user.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('users', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -34,6 +34,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('users');
   }
 };
