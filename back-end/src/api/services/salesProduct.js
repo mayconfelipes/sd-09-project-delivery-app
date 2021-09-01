e9736aa8f367c133ccaef7b991a172595a9af55c
edison-ps <edison@osite.com.br>
2021-09-01 10:47:25 -0300

    [FEAT][Req-20-21][Back-end venda][V1.02]

--- back-end/src/api/services/salesProduct.js
+++ back-end/src/api/services/salesProduct.js
@@ -4,8 +4,7 @@ const { messageError } = require('../middwares/errors');
 
 const { SALE_PRODUCT_NOT_CREATED, SALE_PRODUCT_NOT_EXIST } = require('../middwares/errorMessages');
 
-const { INTERNAL_ERROR_STATUS } = require('../middwares/httpStatus');
-const { NOT_FOUND_STATUS } = require('../../../../../sd-09-project-blogs-api/middwares/httpStatus');
+const { INTERNAL_ERROR_STATUS, NOT_FOUND_STATUS } = require('../middwares/httpStatus');
 
 const create = async (saleProduct) => {
   const { saleId, productId, quantity } = saleProduct;
