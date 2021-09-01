f4027c546dd1433405c5d1df5b19099e7f2c774c
Breno <blf@ic.ufal.br>
2021-09-01 15:11:57 -0300

    fix lint

--- back-end/src/api/service/Login.js
+++ back-end/src/api/service/Login.js
@@ -1,12 +1,12 @@
-const boom = require("@hapi/boom");
-const jwt = require("jsonwebtoken");
-const fs = require("fs");
-const md5 = require("md5");
-const { user } = require("../../database/models");
+const boom = require('@hapi/boom');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const md5 = require('md5');
+const { user } = require('../../database/models');
 
 const createToken = (email) => {
   const secret = fs
-    .readFileSync("jwt.evaluation.key", { encoding: "utf-8" })
+    .readFileSync('jwt.evaluation.key', { encoding: 'utf-8' })
     .trim();
 
   const token = jwt.sign(email, secret);
@@ -18,7 +18,7 @@ const login = async ({ email, password }) => {
   const loginUser = await user.findOne({ where: { email } });
 
   if (!loginUser || loginUser.password !== md5(password)) {
-    throw boom.notFound("Invalid data");
+    throw boom.notFound('Invalid data');
   }
 
   const token = createToken(email);
--- back-end/src/api/service/Register.js
+++ back-end/src/api/service/Register.js
@@ -1,6 +1,6 @@
 const boom = require('@hapi/boom');
 const md5 = require('md5');
-const { createToken } = require('../service/Login');
+const { createToken } = require('./Login');
 
 const registerVerify = require('./utils/registerSchema');
 const { user } = require('../../database/models');
