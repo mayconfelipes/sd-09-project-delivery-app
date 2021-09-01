dd5ece825085760385bcc4d98757f59d2fcf059a
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-09-01 10:47:24 -0300

    getAll sales

---
+++ db.example copy.sql
@@ -0,0 +1,5 @@
+insert into sales(id, user_id, seller_id, total_price, delivery_address, delivery_number, sale_date)  values( 1, 1, 33, 555, '2076', '35617070', 'hoje');
+insert into sales(id, user_id, seller_id, total_price, delivery_address, delivery_number, sale_date)  values( 2, 1, 33, 724, '1112', '35627070', 'amanha');
+insert into sales(id, user_id, seller_id, total_price, delivery_address, delivery_number, sale_date)  values( 3, 1, 33, 312, '76', '35636070', 'depoisdeamanhã');
+insert into sales(id, user_id, seller_id, total_price, delivery_address, delivery_number, sale_date)  values( 4, 1, 33, 5555, '184', '70707070', 'antionti');
+insert into sales(id, user_id, seller_id, total_price, delivery_address, delivery_number, sale_date)  values( 5, 1, 33, 3, '999', 'senaoconeguir,70 denovo', 'agora');
