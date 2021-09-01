36ab5fb90c17fe25b961ae060a37a81d84e8c9fb
Fabio <fabiohigoralm2@gmail.com>
2021-09-01 19:18:04 -0300

    falta 14 e 13

--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './NavBar.css';
 
 function NavBar(props) {
@@ -26,31 +26,31 @@ function NavBar(props) {
         { userType === 'customer' && (
           <>
             <li>
-              <NavLink
+              <Link
                 to="/customer/products"
                 data-testid="customer_products__element-navbar-link-products"
               >
                 Produtos
-              </NavLink>
+              </Link>
             </li>
             <li>
-              <NavLink
+              <Link
                 to="/customer/orders"
                 data-testid="customer_products__element-navbar-link-orders"
               >
                 Meus pedidos
-              </NavLink>
+              </Link>
             </li>
           </>
         ) }
         { userType === 'seller' && (
           <li>
-            <NavLink
+            <Link
               to="/seller/orders"
               data-testid="customer_products__element-navbar-link-orders"
             >
               Meus pedidos
-            </NavLink>
+            </Link>
           </li>
         ) }
         { userType === 'admin' && (
--- front-end/src/components/ProductsCard.js
+++ front-end/src/components/ProductsCard.js
@@ -41,8 +41,7 @@ const ProductsCard = ({ product }) => {
         className="card_price"
         data-testid={ `customer_products__element-card-price-${id}` }
       >
-        R$
-        { price.toFixed(2).toString().replace('.', ',') }
+        { price.replace(/\./, ",") }
       </div>
       <img
         data-testid={ `customer_products__img-card-bg-image-${id}` }
