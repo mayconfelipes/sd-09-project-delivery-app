c43df8b7099c2b7ddf8e089d0d96ffbdbdf5760d
JRGCast <joao.ralph1@gmail.com>
2021-09-01 20:00:01 -0300

    pequenas alteraçoes feitas pro lint, passando ate o 16 ainda

--- back-end/src/api/service/productsServices.js
+++ back-end/src/api/service/productsServices.js
@@ -2,10 +2,8 @@
 
 const { product } = require('../../database/models');
 
-const getAllProducts = async () => {
-  const products = await product.findAll({});
-  return products
-};
+const getAllProducts = async () => product.findAll({});
+
 const getProductById = async (id) => product.findOne({ where: { id } });
 
 module.exports = { getAllProducts, getProductById };
