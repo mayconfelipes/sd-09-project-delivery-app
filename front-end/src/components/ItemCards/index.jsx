5e8c75dd33088c5fefea11bed9297891c6ba4ed8
felipe <felipesantiagolds@gmail.com>
2021-09-01 14:40:46 -0300

    Fix lint register

--- front-end/src/components/ItemCards/index.jsx
+++ front-end/src/components/ItemCards/index.jsx
@@ -4,7 +4,7 @@ import Button from '../Button';
 
 const ItemCard = ({ list }) => (
   list.length ? (
-    <div>
+    <div className="card-item">
       { list.map(({ id, name, price, urlImage }) => (
         <div key={ id }>
           <img
