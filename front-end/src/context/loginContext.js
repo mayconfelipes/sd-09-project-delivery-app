cd00ac24b673a881d56c96c04c6c7550ddd02fa4
JRGCast <joao.ralph1@gmail.com>
2021-09-01 15:26:41 -0300

    resolvido parcialmente front

--- front-end/src/context/loginContext.js
+++ front-end/src/context/loginContext.js
@@ -1,9 +1,6 @@
 import { node } from 'prop-types';
-<<<<<<< HEAD
 import React, { createContext, useState } from 'react';
-=======
 import { fetchToLogin, fetchToRegister } from '../services/api';
->>>>>>> b80bfd10e03aebc2df878b433c5f6c7f30271177
 
 export const LoginContext = createContext();
 
@@ -16,40 +13,6 @@ export const LoginProvider = ({ children }) => {
   const [role, setRole] = useState('');
   const [cart, setCart] = useState([]);
 
-<<<<<<< HEAD
-  const fetchToLogin = () => {
-    const body = {
-      email,
-      password,
-    };
-
-    const myHeadersToLogin = {
-      // 'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    };
-
-    fetch('http://localhost:3001/login', {
-      headers: myHeadersToLogin,
-      method: 'POST',
-      body: JSON.stringify(body),
-    })
-      .then((res) => res.json())
-      .then((response) => {
-        console.log(response);
-        if (response.message) {
-          setInvalidUser(true);
-        } else {
-          console.log(response);
-          localStorage.setItem('User', JSON.stringify(response));
-          setRedirectTo(true);
-          setRole(response.role);
-        }
-      });
-  };
-
-  const handleClickLogin = async () => {
-    await fetchToLogin();
-=======
   const handleClickLogin = async () => {
     console.log('to aq');
     await fetchToLogin(email, password, setInvalidUser, setRedirectTo);
@@ -58,7 +21,6 @@ export const LoginProvider = ({ children }) => {
   const handleClickRegister = async () => {
     console.log('request /register');
     await fetchToRegister({ name, email, password }, setInvalidUser, setRedirectTo);
->>>>>>> b80bfd10e03aebc2df878b433c5f6c7f30271177
   };
 
   const contextValue = {
