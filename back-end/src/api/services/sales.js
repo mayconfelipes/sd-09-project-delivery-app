dfbfee91d49c550bcb1755c3136a921901ba39a9
Felipe Pimentel <lipe.pimentel89@gmail.com>
2021-09-02 15:47:25 -0300

    lint

--- back-end/src/api/services/sales.js
+++ back-end/src/api/services/sales.js
@@ -1,42 +1,44 @@
 const { User, Sale, Product, SalesProduct } = require('../../database/models');
 
 const addProductsToSale = async (saleId, products) => {
-  for (const { productId, quantity } of products) {
-    await SalesProduct.create({ SaleId: saleId, ProductId: productId, quantity });
-  }
+  const result = await Promise
+    .all(products.map(async ({ productId, quantity }) => SalesProduct
+      .create({ SaleId: saleId, ProductId: productId, quantity })));
+  return result;
 };
 
 const getOrderById = async (id) => {
   const { dataValues: orderById } = await Sale.findByPk(id, {
     include: [
       { model: User, as: 'user', attributes: { exclude: ['id', 'password', 'role'] } },
-      { model: User, as: 'seller', attributes: { exclude: ['id', 'password', 'role'] }},
+      { model: User, as: 'seller', attributes: { exclude: ['id', 'password', 'role'] } },
       { model: Product, as: 'products', through: { attributes: ['quantity'] } },
     ],
   });
   orderById.products.map((product) => {
-    product.dataValues.quantity = product.dataValues.SalesProduct.quantity;
-    delete product.dataValues.SalesProduct;
-    return product;
+    const updatedProduct = product;
+    updatedProduct.dataValues.quantity = updatedProduct.dataValues.SalesProduct.quantity;
+    delete updatedProduct.dataValues.SalesProduct;
+    return updatedProduct;
   });
   return orderById;
 };
 
 const newOrder = async ({ products, ...orderWithoutProducts }) => {
-  const { dataValues: newOrder } = await Sale.create({ ...orderWithoutProducts });
-  await addProductsToSale(newOrder.id, products);
-  const registeredOrder = await getOrderById(newOrder.id);
+  const { dataValues: order } = await Sale.create({ ...orderWithoutProducts });
+  await addProductsToSale(order.id, products);
+  const registeredOrder = await getOrderById(order.id);
   return registeredOrder;
 };
 
 const getAllOrders = async (userId) => {
   const allOrders = await Sale.findAll({ where: { userId } });
-  return Promise.all(allOrders.map(async (order) => await getOrderById(order.id)));
+  return Promise.all(allOrders.map(async (order) => getOrderById(order.id)));
 };
 
 const getAllSales = async (sellerId) => {
   const allSales = await Sale.findAll({ where: { sellerId } });
-  return Promise.all(allSales.map(async (sale) => await getOrderById(sale.id)));
+  return Promise.all(allSales.map(async (sale) => getOrderById(sale.id)));
 };
 
 const updateOrderStatus = async (id, status) => {
