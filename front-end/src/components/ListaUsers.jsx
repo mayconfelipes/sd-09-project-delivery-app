1cabb0383afac7d9a3c7cab1927d6218c9954985
Raquel Picanço <picanco.raquel@gmail.com>
2021-09-02 15:51:47 -0400

    feat: create table of users (no req yet)

--- front-end/src/components/ListaUsers.jsx
+++ front-end/src/components/ListaUsers.jsx
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function ListaUsers() {
+  const [users] = useState([]);
+
   return (
-    <div />
+    <section>
+      Lista de usuários
+      <table>
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Tipo</th>
+            <th>Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            users.map(({ name, email, role }, index) => (
+              <tr key={ email }>
+                <td
+                  data-testid={ `admin_manage__element-user-table-item-number-${index}` }
+                >
+                  { index + 1 }
+                </td>
+                <td
+                  data-testid={ `admin_manage__element-user-table-name-${index}` }
+                >
+                  { name }
+                </td>
+                <td
+                  data-testid={ `admin_manage__element-user-table-email-${index}` }
+                >
+                  { email }
+                </td>
+                <td
+                  data-testid={ `admin_manage__element-user-table-role-${index}` }
+                >
+                  { role }
+                </td>
+                <td
+                  data-testid={ `admin_manage__element-user-table-remove-${index}` }
+                >
+                  Excluir
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </section>
   );
 }
