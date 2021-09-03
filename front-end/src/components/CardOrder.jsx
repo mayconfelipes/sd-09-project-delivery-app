20b33c17852cbeda8200e5c01237208b3db6141a
Uilton <itoclick@gmail.com>
2021-09-03 02:19:51 -0300

    tela de vendas ok

--- front-end/src/components/CardOrder.jsx
+++ front-end/src/components/CardOrder.jsx
@@ -1,28 +1,39 @@
-import React, { useContext } from 'react';
-import Context from '../context/Context';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function CardOrder() {
-// PENDENTE - PREPARANDO - ENTREGUE
-  const { products } = useContext(Context);
-  console.log(products);
+export default function CardOrder({ venda }) {
+  const { id, status, saleDate, totalPrice } = venda;
+  const formatedDate = new Date(saleDate).toLocaleDateString('pt-BR');
+
   return (
     <div className="corpo_card">
       <div className="div_pedido">
         <span>Pedido</span>
-        <span data-testid="customer_orders__element-order-id-<id>">0001</span>
+        <span data-testid="customer_orders__element-order-id-<id>">{id}</span>
       </div>
 
       <button
         type="button"
         data-test-id=" customer_orders__element-delivery-status-<id>"
       >
-        situação
+        {status}
       </button>
 
       <div className="data_valor">
-        <span data-testid="customer_orders__element-order-date-<id>">08/02/02</span>
-        <span>R$totalpedido</span>
+        <span
+          data-testid="customer_orders__element-order-date-<id>"
+        >
+          {formatedDate}
+        </span>
+        <span>{`R$ ${totalPrice}`}</span>
       </div>
     </div>
   );
 }
+CardOrder.propTypes = {
+  venda: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    status: PropTypes.string.isRequired,
+    saleDate: PropTypes.string.isRequired,
+    totalPrice: PropTypes.string.isRequired,
+  }).isRequired };
--- front-end/src/pages/Orders.jsx
+++ front-end/src/pages/Orders.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Header from '../components/Header';
 import CardOrder from '../components/CardOrder';
 import '../styles/Orders.css';
 
 export default function Orders() {
+  const [order, setOrder] = useState([]);
+  const [userData] = useState(JSON.parse(localStorage.getItem('user')));
+  const { token } = userData;
+  const config = { headers: { Authorization: `${token}` } };
+
+  useEffect(
+    () => {
+      const ORDERS_ENDPOINT = 'http://localhost:3001/api/sales';
+      const fetchOrders = async () => {
+        await axios.get(ORDERS_ENDPOINT, config)
+          .then((d) => setOrder(d.data));
+      };
+      fetchOrders();
+    }, [config],
+  );
+
   return (
     <main>
       <Header />
       <section>
-        <CardOrder />
+        {order.map((Item) => <CardOrder key={ Item.id } venda={ Item } />)}
       </section>
     </main>
   );
