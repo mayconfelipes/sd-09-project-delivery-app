bd96ad3b3ae91aecf64367bb82e7e989758f7144
Luis Felipe Avila <lfelipeavila@outlook.com>
2021-09-02 23:53:16 -0300

    css login

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -2,7 +2,8 @@ import React, { useContext, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 import Context from '../context/Context';
-import '../App.css';
+import './Login.css';
+import cervaApp from '../images/cerva-app.png';
 
 function Login() {
   const history = useHistory();
@@ -58,47 +59,58 @@ function Login() {
   };
 
   return (
-    <div className="App">
-      <h1>Login</h1>
-      <form className="login-form">
-        <input
-          type="email"
-          name="login-email"
-          onChange={ validateEmail }
-          data-testid="common_login__input-email"
-          placeholder="email@email.com"
-          className="email-password"
-        />
-        <input
-          type="password"
-          name="login-password"
-          onChange={ validatePassword }
-          data-testid="common_login__input-password"
-          placeholder="Digite sua senha"
-          className="email-password"
-        />
-        <button
-          type="submit"
-          onClick={ validateUser }
-          data-testid="common_login__button-login"
-          disabled={ email === '' || password === '' }
-          className="btn-submit"
-        >
-          LOGIN
-        </button>
-      </form>
-      <button
-        type="button"
-        className="sign-btn"
-        data-testid="common_login__button-register"
-        onClick={ () => history.push('/register') }
-      >
-        Ainda não tenho conta
-      </button>
-      <div>
-        <p data-testid="common_login__element-invalid-email">
-          { errorMessage }
-        </p>
+    <div className="wrapper fadeInDown">
+      <div className="formContent">
+        <div className="fadeIn first">
+          <img
+            src={ cervaApp }
+            className="icon"
+            alt="icon"
+          />
+          <br />
+          <h1>Cerva App</h1>
+          <h2>Login</h2>
+        </div>
+        <form className="">
+          <input
+            type="email"
+            name="login-email"
+            onChange={ validateEmail }
+            data-testid="common_login__input-email"
+            placeholder="email@email.com"
+            className="email-password fadeIn second"
+          />
+          <input
+            type="password"
+            name="login-password"
+            onChange={ validatePassword }
+            data-testid="common_login__input-password"
+            placeholder="Digite sua senha"
+            className="email-password fadeIn third"
+          />
+          <button
+            type="submit"
+            onClick={ validateUser }
+            data-testid="common_login__button-login"
+            disabled={ email === '' || password === '' }
+            className="btn-submit fadeIn fourth"
+          >
+            LOGIN
+          </button>
+        </form>
+        <div className="formFooter underlineHover">
+          <button
+            type="button"
+            className="fadeIn five"
+            data-testid="common_login__button-register"
+            onClick={ () => history.push('/register') }
+          >
+            Ainda não tenho conta
+          </button>
+          <p data-testid="common_login__element-invalid-email">
+            { errorMessage }
+          </p>
+        </div>
       </div>
     </div>
   );
