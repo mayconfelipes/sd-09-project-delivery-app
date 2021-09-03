b8f61c11323636c5a0c0f48f995ef8dab41e6014
MarceloBorges <melombm@hotmail.com>
2021-09-02 22:51:09 -0500

    requisio 6 e 7

--- front-end/src/hooks/useRegisterInfo.js
+++ front-end/src/hooks/useRegisterInfo.js
@@ -4,6 +4,7 @@ const useRegisterInfo = () => {
   const [registerInfo, setRegisterInfo] = useState(() => ({
     userName: '', email: '', password: '',
   }));
+
   const handleFieldsChange = ({ target: { name, value } }) => {
     setRegisterInfo((currentState) => ({ ...currentState, [name]: value }));
   };
