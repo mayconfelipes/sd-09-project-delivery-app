4acea38402ba0bfd96c8c9279f55edd55e98f2ff
Rafael Reis <rafaelrnascimento2006@gmail.com>
2021-09-03 17:52:43 -0300

    feat-admin_page

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -17,47 +17,47 @@ export const login = async (body, successCb, errorCb) => {
 };
 
 export const registerUser = (body) => axios.post(`${url}/user`, body)
-  .then(({ data }) => data)
-  .catch((error) => error.response.data);
+  .then(({ data }) => (data))
+  .catch((e) => ({ error: e.response.data }));
 
 export const createUser = (body) => axios.post(`${url}/user/admin`, body, {
   headers: { authorization: JSON.parse(localStorage.user).token },
-}).then(({ data }) => data)
-  .catch((error) => error.response.data);
-
-export const deleteUser = (id) => axios.delete(`${url}/user/${id}`)
-  .then(({ data }) => data)
-  .catch((error) => error.response.data);
+}).then(({ data }) => (data))
+  .catch((e) => ({ error: e.response.data }));
+export const deleteUser = (id) => axios.delete(`${url}/user/${id}`, {
+  headers: { authorization: JSON.parse(localStorage.user).token },
+}).then(({ data }) => (data))
+  .catch((e) => ({ error: e.response.data }));
 
 export const getProducts = () => axios.get(`${url}/product`)
-  .then(({ data }) => data)
-  .catch((error) => error.response.data);
+  .then(({ data }) => (data))
+  .catch((e) => ({ error: e.response.data }));
 
 export const getSalesBySeller = (id) => axios.get(`${url}/sale/seller/${id}`, {
   headers: { authorization: JSON.parse(localStorage.user).token },
-}).then(({ data }) => data)
-  .catch((error) => error.response.data);
+}).then(({ data }) => (data))
+  .catch((e) => ({ error: e.response.data }));
 
 export const getSaleByid = (id) => axios.get(`${url}/sale/${id}`, {
   headers: { authorization: JSON.parse(localStorage.user).token },
-}).then(({ data }) => data)
-  .catch((error) => error.response.data);
+}).then(({ data }) => (data))
+  .catch((e) => ({ error: e.response.data }));
 
 export const createSale = (body) => axios.post(`${url}/sale`, body, {
   headers: { authorization: JSON.parse(localStorage.user).token },
-}).then(({ data }) => data)
-  .catch((error) => error.response.data);
+}).then(({ data }) => (data))
+  .catch((e) => ({ error: e.response.data }));
 
 export const getUsers = () => axios.get(`${url}/user`)
-  .then(({ data }) => data)
-  .catch((error) => error.response.data);
+  .then(({ data }) => (data))
+  .catch((e) => ({ error: e.response.data }));
 
 export const getAllSales = () => axios.get(`${url}/sale`, {
   headers: { authorization: JSON.parse(localStorage.user).token },
-}).then(({ data }) => data)
-  .catch((error) => error.response.data);
+}).then(({ data }) => (data))
+  .catch((e) => ({ error: e.response.data }));
 
 export const getUserByid = (id) => axios.get(`${url}/user/${id}`, {
   headers: { authorization: JSON.parse(localStorage.user).token },
-}).then(({ data }) => data)
-  .catch((error) => error.response.data);
+}).then(({ data }) => (data))
+  .catch((e) => ({ error: e.response.data }));
