68349856cb00f0666e55cce0a853ee8628d1ae85
Raquel Picanço <picanco.raquel@gmail.com>
2021-09-03 21:46:38 -0400

    fix: add data-testids and fix migration type

--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -1,28 +1,69 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import { Redirect, useHistory } from 'react-router-dom';
+import axios from 'axios';
+
 import Header from '../components/Header';
-import Context from '../context/Context';
+import ProductsContext from '../context/ProductsContext';
 
 const Checkout = () => {
-  const { products, setProducts } = useContext(Context);
-  // const [seller, setSeller] = useState('');
-  const [address, setAddress] = useState('');
+  const { products, setProducts } = useContext(ProductsContext);
+
+  const history = useHistory();
+
+  const [sellers, setSellers] = useState([]);
+  const [deliveryAddress, setDeliveryAddress] = useState('');
   const [total, setTotal] = useState(JSON.parse(localStorage.getItem('total')) || 0);
-  const [addressNumber, setAddressNumber] = useState('');
+  const [deliveryNumber, setDeliveryNumber] = useState('');
+  const [sellerId, setSelletId] = useState('');
+  const [user] = useState(JSON.parse(localStorage.getItem('user')));
+  const [error, setError] = useState('');
+
+  const renderSellersDropdown = () => (
+    <select
+      name="employees"
+      id="seller"
+      data-testid="customer_checkout__select-seller"
+      onChange={ (e) => setSelletId(e.target.value) }
+    >
+      <option value="">Selecione uma pessoa vendedora</option>
+      {
+        sellers.map(({ id, name }) => (
+          <option value={ id } key={ name }>{name}</option>
+        ))
+      }
+    </select>
+  );
 
   const handleChange = ({ target: { name, value } }) => {
     switch (name) {
     case 'address':
-      return setAddress(value);
+      return setDeliveryAddress(value);
     case 'number':
-      return setAddressNumber(value);
+      return setDeliveryNumber(value);
     default:
       return '';
     }
   };
 
-  console.log(address, addressNumber);
+  useEffect(
+    () => {
+      if (user) {
+        const fetchSellers = async () => {
+          const GET_SELLERS_ENDPOINT = 'http://localhost:3001/api/users';
+          const config = {
+            headers: { Authorization: `${user.token}` },
+          };
 
-  // console.log(products);
+          await axios.get(GET_SELLERS_ENDPOINT, config)
+            .then(
+              ({ data }) => setSellers(data),
+              () => setError('Falha ao listar pessoas vendedoras'),
+            );
+        };
+        fetchSellers();
+      }
+    }, [user],
+  );
 
   const handleClick = (productId, productPrice, productQuantity) => {
     const list = products.filter((product) => {
@@ -37,12 +78,51 @@ const Checkout = () => {
     localStorage.setItem('products', JSON.stringify(list));
   };
 
+  const submitSale = async () => {
+    const POST_SALE_ENDPOINT = 'http://localhost:3001/api/sales';
+    const payload = {
+      userId: user.id,
+      sellerId,
+      totalPrice: total,
+      deliveryAddress,
+      deliveryNumber,
+      status: 'Pendente',
+      products,
+    };
+    const config = {
+      headers: { Authorization: `${user.token}` },
+    };
+
+    await axios.post(POST_SALE_ENDPOINT, payload, config)
+      .then(
+        ({ data: { id } }) => history.push(`/customer/orders/${id}`),
+        () => setError('Falha ao criar pedido D='),
+      );
+  };
+
+  if (!user) {
+    localStorage.clear();
+    return (<Redirect
+      to={ {
+        pathname: '/login',
+        state: {
+          error: 'Você não tem permissão para accessar essa página!',
+        },
+      } }
+
+    />);
+  }
+
   return (
     <section className="checkout">
       <Header />
-      <span className="checkout-order">Finalizar Pedido</span>
-      <section className="items">
-        <table>
+      <h2 className="checkout-order">Finalizar Pedido</h2>
+      {
+        error.length > 0
+        && <span>{error}</span>
+      }
+      <table>
+        <thead>
           <tr>
             <td>Item</td>
             <td>Descrição</td>
@@ -51,22 +131,47 @@ const Checkout = () => {
             <td>Subtotal</td>
             <td>Remover Item</td>
           </tr>
+        </thead>
+        <tbody>
           { products.map((product, index) => (
             <tr key={ product.id }>
-              <td>
+              <td
+                data-testid={
+                  `customer_checkout__element-order-table-item-number-${index}`
+                }
+              >
                 { index + 1 }
               </td>
-              <td>
+              <td
+                data-testid={
+                  `customer_checkout__element-order-table-name-${index}`
+                }
+              >
                 { product.name }
               </td>
-              <td>
+              <td
+                data-testid={
+                  `customer_checkout__element-order-table-quantity-${index}`
+                }
+              >
                 { product.quantity }
               </td>
-              <td>
-                { product.price }
+              <td
+                data-testid={
+                  `customer_checkout__element-order-table-unit-price-${index}`
+                }
+              >
+                { product.price.replace('.', ',') }
               </td>
-              <td>
-                { parseFloat(product.price * product.quantity).toFixed(2) }
+              <td
+                data-testid={
+                  `customer_checkout__element-order-table-sub-total-${index}`
+                }
+              >
+                {
+                  parseFloat(product.price * product.quantity)
+                    .toFixed(2).replace('.', ',')
+                }
               </td>
               <td>
                 <button
@@ -74,30 +179,32 @@ const Checkout = () => {
                   onClick={
                     () => handleClick(product.id, product.price, product.quantity)
                   }
+                  data-testid={
+                    `customer_checkout__element-order-table-remove-${index}`
+                  }
                 >
                   REMOVER
                 </button>
               </td>
             </tr>
           )) }
+        </tbody>
 
-        </table>
-        <span className="total">
+      </table>
+      <section>
+        <span
+          className="total"
+          data-testid="customer_checkout__element-order-total-price"
+        >
           Total: R$
-          { parseFloat(total).toFixed(2) }
+          { parseFloat(total).toFixed(2).replace('.', ',') }
         </span>
       </section>
-      <span className="checkout-details">Detalhes e Endereço para Entrega</span>
+      <h2 className="checkout-details">Detalhes e Endereço para Entrega</h2>
       <section className="details">
         <label htmlFor="seller">
           P.Vendedora Responsável:
-          <select
-            name="employees"
-            id="seller"
-            data-testid="customer_checkout__select-seller"
-          >
-            <option value="fulanaDeTal">Fulana de Tal</option>
-          </select>
+          { renderSellersDropdown() }
         </label>
         <label htmlFor="addressInput">
           Endereço
@@ -124,6 +231,7 @@ const Checkout = () => {
         type="button"
         name="Finalizar pedido"
         data-testid="customer_checkout__button-submit-order"
+        onClick={ submitSale }
       >
         Finalizar pedido
       </button>
