3c010fa5d5df47f9a5d0a3726cf54c0a7dbde874
Ana Luiza Machado Salgado <analuizamsalgado@gmail.com>
2021-09-06 18:34:59 -0300

    fixing header

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 
 const Header = () => {
   const user = JSON.parse(localStorage.getItem('user'));
+  const [firstNavBarText, setFirstNavBarText] = useState('');
+  const [secondNavBarLink, setSecondNavBarLink] = useState(false);
   const History = useHistory();
 
   function handleLogout() {
     localStorage.clear();
     History.push('/login');
   }
+
+  function renderSecondNavBarLink() {
+    return (
+      <Link
+        className="noUnderline"
+        to="/customer/orders"
+        data-testid="customer_products__element-navbar-link-orders"
+      >
+        MEUS PEDIDOS
+      </Link>
+    );
+  }
+
+  useEffect(() => {
+    if (History.location.pathname === '/admin/manage') {
+      setFirstNavBarText('GERENCIAR USUÁRIOS');
+    }
+    if (History.location.pathname === '/seller/orders') {
+      setFirstNavBarText('PEDIDOS');
+    }
+    setFirstNavBarText('PRODUTOS');
+    setSecondNavBarLink(true);
+    console.log(History.location.pathname);
+  }, [History]);
 
   return (
     <div className="navBar">
@@ -18,15 +44,9 @@ const Header = () => {
           to="/customer/products"
           data-testid="customer_products__element-navbar-link-products"
         >
-          PRODUTOS
+          { firstNavBarText }
         </Link>
-        <Link
-          className="noUnderline"
-          to="/customer/orders"
-          data-testid="customer_products__element-navbar-link-orders"
-        >
-          MEUS PEDIDOS
-        </Link>
+        { secondNavBarLink && renderSecondNavBarLink() }
       </div>
       <div className="rightHeader">
         <p data-testid="customer_products__element-navbar-user-full-name">
