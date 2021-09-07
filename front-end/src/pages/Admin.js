6b079c9ae2a2337212d06fec80e9613f2e5e7710
Ana Luiza Machado Salgado <analuizamsalgado@gmail.com>
2021-09-06 22:00:42 -0300

    fix lint

--- front-end/src/pages/Admin.js
+++ front-end/src/pages/Admin.js
@@ -1,57 +1,55 @@
 import React from 'react';
 import Header from '../components/Header';
 
-const Admin = () => {
-  return (
-    <div>
-      <Header />
-      <h3>Cadastrar novo usuário</h3>
-      <form>
-        <label htmlFor="userName">
-          Nome
-          <input
-            id="userName"
-            name="userName"
-            placeholder="Nome e sobrenome"
-            data-testid="admin_manage__input-name"
-          />
-        </label>
-        <label htmlFor="userEmail">
-          Email
-          <input
-            id="userEmail"
-            name="userEmail"
-            data-testid="admin_manage__input-email"
-          />
-        </label>
-        <label htmlFor="userPassword">
-          Senha
-          <input
-            id="userPassword"
-            name="userPassword"
-            data-testid="admin_manage__input-password"
-          />
-        </label>
-        <label htmlFor="userRole">
-          Tipo
-          <select
-            id="userRole"
-            name="userRole"
-            data-testid="admin_manage__select-role"
-          >
-            <option key="seller" value="seller">Vendedor</option>
-          </select>
-        </label>
-        <button
-          type="submit"
-          data-testid="admin_manage__button-register"
-          disabled
+const Admin = () => (
+  <div>
+    <Header />
+    <h3>Cadastrar novo usuário</h3>
+    <form>
+      <label htmlFor="userName">
+        Nome
+        <input
+          id="userName"
+          name="userName"
+          placeholder="Nome e sobrenome"
+          data-testid="admin_manage__input-name"
+        />
+      </label>
+      <label htmlFor="userEmail">
+        Email
+        <input
+          id="userEmail"
+          name="userEmail"
+          data-testid="admin_manage__input-email"
+        />
+      </label>
+      <label htmlFor="userPassword">
+        Senha
+        <input
+          id="userPassword"
+          name="userPassword"
+          data-testid="admin_manage__input-password"
+        />
+      </label>
+      <label htmlFor="userRole">
+        Tipo
+        <select
+          id="userRole"
+          name="userRole"
+          data-testid="admin_manage__select-role"
         >
-          CADASTRAR
-        </button>
-      </form>
-    </div>
-  );
-};
+          <option key="seller" value="seller">Vendedor</option>
+        </select>
+      </label>
+      <button
+        type="submit"
+        data-testid="admin_manage__button-register"
+        disabled
+      >
+        CADASTRAR
+      </button>
+    </form>
+  </div>
+);
 
 export default Admin;
