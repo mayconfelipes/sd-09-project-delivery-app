26e28476e7dff557a4bd336ab4e7963b923a48f0
JRGCast <joao.ralph1@gmail.com>
2021-09-08 19:56:43 -0300

    desisto

--- back-end/src/database/models/product.js
+++ back-end/src/database/models/product.js
@@ -7,22 +7,3 @@ module.exports = (sequelize, DataTypes) => {
 
   return Product;
 };
-
-// module.exports = (sequelize, DataTypes) => {
-//   const product = sequelize.define('product', {
-//     name: DataTypes.STRING,
-//     price: DataTypes.DECIMAL,
-//     url_image: DataTypes.STRING
-//   },
-//     {
-//       timestamps: false,
-//       tableName: 'products'
-//     });
-
-//   product.associate = (models) => {
-//     product.belongsToMany(models.sale, { through: models.salesProduct });
-//     product.hasMany(models.salesProduct);
-//   };
-
-//   return product;
-// };
--- back-end/src/database/models/salesproduct.js
+++ back-end/src/database/models/salesproduct.js
@@ -1,34 +1,26 @@
 module.exports = (sequelize, DataTypes) => {
-  const salesProduct = sequelize.define('salesProduct', {
-    saleId: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'sale',
-        key: 'id'
-      }
-    },
-    productId: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'product',
-        key: 'id'
-      }
-    },
+  const SalesProduct = sequelize.define('salesProducts', {
     quantity: DataTypes.INTEGER
-  },
-    {
-      timestamps: false,
-      underscored: true,
-      tableName: 'salesProducts'
-    });
+  }, { timestamp: false });
 
-  salesProduct.associate = (models) => {
-    salesProduct.belongsTo(models.sale);
-    salesProduct.belongsTo(models.product);
+  SalesProduct.associate = (models) => {
+    models.sale.belongsToMany(models.product, {
+      as: 'products',
+      through: "salesProducts",
+      foreignKey: 'sale_id',
+      otherkey: 'product_id',
+    });
+    models.product.belongsToMany(models.sale, {
+      as: 'sales',
+      through: "salesProducts",
+      foreignKey: 'product_id',
+      otherkey: 'sale_id',
+    });
   };
 
-  return salesProduct;
+  return SalesProduct;
 };
+
 // module.exports = (sequelize, DataTypes) => {
 //   const salesProduct = sequelize.define('salesProduct', {
 //     saleId: {
