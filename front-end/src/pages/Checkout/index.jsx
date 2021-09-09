3fd37bee5386719e041321cc9d8226b2a759f255
Enio Nicael <enionicael@gmail.com>
2021-09-09 15:26:53 -0300

    Adicao de imagem de logo na tela de login.

--- front-end/src/pages/Checkout/index.jsx
+++ front-end/src/pages/Checkout/index.jsx
@@ -3,7 +3,8 @@ import { useHistory } from 'react-router-dom';
 import { useDeliveryContext } from '../../context/deliveryProvider';
 import OrderLIst from '../../components/checkout/OrderLIst';
 import AddressDetails from '../../components/checkout/AddressDetails';
-import Header from '../../components/Header';
+// import Header from '../../components/Header';
+import NavBar from '../../components/Navbar';
 import api from '../../service/axiosApi';
 import './style.css';
 
@@ -45,25 +46,25 @@ const Checkout = () => {
   };
 
   return (
-    <div className="checkout-container">
-      <header>
-        <Header />
-      </header>
-      <div className="checkout-main-container">
-        <h2>Finalizar pedidos</h2>
-        <OrderLIst />
-        <h2>Detalhes e Endereço para Entrega</h2>
-        <AddressDetails names={ sellers } />
+    <>
+      <NavBar />
+      <div className="checkout-container">
+        <div className="checkout-main-container">
+          <h2>Finalizar pedidos</h2>
+          <OrderLIst />
+          <h2>Detalhes e Endereço para Entrega</h2>
+          <AddressDetails names={ sellers } />
+        </div>
+        <button
+          type="button"
+          className="finalize-order"
+          data-testid="customer_checkout__button-submit-order"
+          onClick={ handeClick }
+        >
+          FINALIZAR PEDIDO
+        </button>
       </div>
-      <button
-        type="button"
-        className="finalize-order"
-        data-testid="customer_checkout__button-submit-order"
-        onClick={ handeClick }
-      >
-        FINALIZAR PEDIDO
-      </button>
-    </div>
+    </>
   );
 };
 
