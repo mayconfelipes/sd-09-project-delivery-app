e6739c86acedeaae63d044dcb8970601f4e6d4b7
brenno-calado <brenno.calado@gmail.com>
2021-09-09 16:38:38 -0300

    conditional context

--- front-end/src/pages/OrderDetail.js
+++ front-end/src/pages/OrderDetail.js
@@ -5,13 +5,15 @@ import CheckoutTable from '../components/CheckoutTable';
 import OrderHeader from '../components/OrderHeader';
 import useOrderDetail from '../hooks/useOrderDetail';
 import Seller from '../context/sellerContext';
+import Customer from '../context/customerContext';
 
 const OrderDetail = () => {
-  const { setShoppingCart } = useContext(Seller);
   const { token } = JSON.parse(localStorage.getItem('user'));
   const History = useHistory();
   const path = History.location.pathname.split('/');
   const id = path[path.length - 1];
+  const role = path[1];
+  const { setShoppingCart } = useContext(role === 'seller' ? Seller : Customer);
   const [setOrderDetail] = useOrderDetail();
 
   useEffect(() => {
