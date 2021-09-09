07e83cbda6fb094c7883fb80fe62ecd08302419f
marcusWittho <mcswitthoeft@gmail.com>
2021-09-09 17:06:14 -0300

    Requisito 40

--- front-end/src/pages/Admin.jsx
+++ front-end/src/pages/Admin.jsx
@@ -1,13 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 import NavBarAdmin from '../components/navBarAdmin';
 
 io('http://localhost:3001');
 
 function Admin() {
-  useEffect(() => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [role, setRole] = useState('seller');
+  const [isValid, setIsValid] = useState(false);
 
-  }, []);
+  useEffect(() => {
+    const magicNumber = 12;
+    const passwordMinLength = 6;
+    const emailRegex = new RegExp(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+    if (emailRegex.test(email)
+      && password.length >= passwordMinLength
+      && name.length >= magicNumber) {
+      return setIsValid(true);
+    }
+
+    setIsValid(false);
+  }, [name, email, password, role]);
 
   return (
     <div>
@@ -21,6 +36,7 @@ function Admin() {
           <input
             id="name"
             type="text"
+            onChange={ (e) => setName(e.target.value) }
             placeholder="Nome e sobrenome"
             data-testid="admin_manage__input-name"
           />
@@ -31,6 +47,7 @@ function Admin() {
           <input
             id="email"
             type="text"
+            onChange={ (e) => setEmail(e.target.value) }
             placeholder="seu-email@site.com.br"
             data-testid="admin_manage__input-email"
           />
@@ -41,6 +58,7 @@ function Admin() {
           <input
             id="senha"
             placeholder="******"
+            onChange={ (e) => setPassword(e.target.value) }
             type="password"
             data-testid="admin_manage__input-password"
           />
@@ -48,7 +66,10 @@ function Admin() {
 
         <label htmlFor="tipo">
           Tipo
-          <select data-testid="admin_manage__select-role">
+          <select
+            data-testid="admin_manage__select-role"
+            onChange={ (e) => setRole(e.target.value) }
+          >
             <option value="administrator">Administrador</option>
             <option value="seller" selected="selected">Vendedor</option>
             <option value="customer">Cliente</option>
@@ -57,18 +78,12 @@ function Admin() {
 
         <button
           type="button"
+          disabled={ !isValid }
           data-testid="admin_manage__button-register"
         >
           Cadastrar
         </button>
       </form>
-
-      <button
-        type="button"
-        onClick={ () => socketRef.current.emit('teste', { teste: 'Olá' }) }
-      >
-        Teste
-      </button>
     </div>
   );
 }
